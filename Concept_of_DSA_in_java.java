ad3231bd7598278a5e55240af7dad4bff577260f
sumit upadhyay <113931544+sumitupadhyay84@users.noreply.github.com>
2025-08-03 02:32:26 +0530

    Update Concept_of_DSA_in_java.java

--- Concept_of_DSA_in_java.java
+++ Concept_of_DSA_in_java.java
@@ -45,3 +45,58 @@ public class Main {
       System.out.println(sb);
   }
 }
+
+// Hashset in java // it is unordered in nature
+HashSet in Java
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class Hashing {
+   public static void main(String args[]) {
+       HashSet<Integer> set = new HashSet<>();
+       //Add
+       set.add(1);
+       set.add(2);
+       set.add(3);
+       set.add(1);
+
+       //Size
+       System.out.println("size of set is : " + set.size());
+
+       //Search
+       if(set.contains(1)) {
+           System.out.println("present");
+       }
+
+       if(!set.contains(6)) {
+           System.out.println("absent");
+       }
+
+       //Delete
+       set.remove(1);
+       if(!set.contains(1)) {
+           System.out.println("absent");
+       }
+
+       //Print all elements
+       System.out.println(set);
+
+       //Iteration - HashSet does not have an order
+       set.add(0);
+       Iterator it = set.iterator();
+        while (it.hasNext()) {                 // it return true untill elenent contains;
+           System.out.print(it.next() + ", "); // it iterate all the element without add the increment order;
+       }
+       System.out.println();
+       //isEmpty
+       if(!set.isEmpty()) {
+           System.out.println("set is not empty");
+       }
+   }
+}
+
+
+
+
